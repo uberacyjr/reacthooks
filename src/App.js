043b372159408949d697232e1cc5c98fb636cbda
Uberacy Tottoli <bera@boldtech.com.br>
2019-02-09 15:36:04 -0200

    Add list of persons and pagination

--- src/App.js
+++ src/App.js
@@ -1,35 +1,97 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function List(props){
-  
-    function addHandler(){
-      props.estado( props.count + parseInt(numero));
-    }
-  const [numero, setNumero] = useState(0);
-
-  return (
-      <React.Fragment>
-          <input type="number" value={numero} onChange={e=>setNumero(e.target.value)} ></input>
-          <button type="button" onClick={addHandler}>GO</button>
-      </React.Fragment>
-  );
-}
-
- function App () {
-
+function App() {
+  const [carregou, setCarregou] = useState(false);
+  const [pessoas, setPessoas] = useState([]);
+  const [count, setCount] = useState(1);
+  const [listPages,setListPage] = useState([1,2,3,4]);
   useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
-  
-  const [count, setCount] = useState(0);
+    personHandler(count);
+  }, []);
+
+  function personHandler(position) {
+    setCarregou(false);
+    setCount(position);
+    fetch("https://reqres.in/api/users?page=" + position)
+      .then(response => response.json())
+      .then(data => {
+        setPessoas(data.data);
+        setCarregou(true);
+      });
+  }
+
+  if (!carregou) {
     return (
-        <div className="App">
-            <h2>Principal {count}</h2>
-            <List count={count} estado={setCount}></List>
+      <div className="mt-5 text-center">
+        <div className="spinner-grow text-primary" role="status">
+          <span className="sr-only">Loading...</span>
         </div>
+        <div className="spinner-grow text-secondary" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-grow text-success" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-grow text-danger" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-grow text-warning" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-grow text-info" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-grow text-light" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="spinner-grow text-dark" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
     );
+  } else {
+    return (
+      <React.Fragment>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Avatar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pessoas.map((p) =>
+              <tr key={p.id}>
+                <th>{p.id}</th>
+                <td>{p.first_name}</td>
+                <td>{p.last_name}</td>
+                <td><img src={p.avatar} alt={"avatar_" + p.id}></img></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        <nav aria-label="...">
+          <ul className="pagination pagination-sm">
+              {
+                listPages.map((l)=>
+                count == l ? 
+                <li class="page-item active" aria-current="page"><span class="page-link" onClick={() => { personHandler(l) }}> {l}<span class="sr-only">(current)</span></span></li>
+                :
+                 <li className="page-item"><a className="page-link" href="#" onClick={() => { personHandler(l) }}>{l}</a></li>
+                )
+              }
+          </ul>
+        </nav>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
+
+
+
